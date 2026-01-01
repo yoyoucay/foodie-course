5aa1545bef7160daca1aa2c8f8e48473403854a1
yoyoucay <44655602+yoyoucay@users.noreply.github.com>
2026-01-02 03:25:33 +0700

    Add MealsPage layout with header, call-to-action, and MealsGrid component; implement MealItem for individual meal display

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
+
 export default function MealsPage() {
     return (
-        <main>
-            <h1>This is meals Page</h1>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>Delicious Meals <span className={classes.highlight}> by you</span></h1>
+                <p>Choose your favorite dishes and enjoy a culinary adventure.</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                    Share your favorite meal recipes with others!
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     );
 }
