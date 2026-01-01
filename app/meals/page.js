eebf3b817d04ce6f4bc160ecb226187ccf2399e9
yoyoucay <44655602+yoyoucay@users.noreply.github.com>
2026-01-02 03:46:33 +0700

    feat: Integrate Suspense for meal loading and remove deprecated loading components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,24 +1,33 @@
-import Link from 'next/link';
-import classes from './page.module.css';
-import MealsGrid from '@/components/meals/meals-grid';
-import { getMeals } from '@/lib/meals';
+import { Suspense } from "react";
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
-    const meals = await getMeals();
-    return (
-        <>
-            <header className={classes.header}>
-                <h1>Delicious Meals <span className={classes.highlight}> by you</span></h1>
-                <p>Choose your favorite dishes and enjoy a culinary adventure.</p>
-                <p className={classes.cta}>
-                    <Link href="/meals/share">
-                    Share your favorite meal recipes with others!
-                    </Link>
-                </p>
-            </header>
-            <main className={classes.main}>
-                <MealsGrid meals={meals} />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>Choose your favorite dishes and enjoy a culinary adventure.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share your favorite meal recipes with others!
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Loading meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
