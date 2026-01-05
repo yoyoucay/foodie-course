ce023c5a4fc4fb407dfd0151abe9973f81783126
yoyoucay <44655602+yoyoucay@users.noreply.github.com>
2026-01-05 10:18:31 +0700

    feat : change storage file using s3 AWS amazon, not using local file anymore. installing package s3 AWS. refactore initdb.js

--- components/meals/meals-item.js
+++ components/meals/meals-item.js
@@ -4,11 +4,12 @@ import Image from 'next/image';
 import classes from './meal-item.module.css';
 
 export default function MealItem({ title, slug, image, summary, creator }) {
+  console.log({image})
   return (
     <article className={classes.meal}>
       <header>
         <div className={classes.image}>
-          <Image src={image} alt={title} fill />
+          <Image src={`https://foodie-course-image.s3.ap-southeast-2.amazonaws.com/${image}`} alt={title} fill />
         </div>
         <div className={classes.headerText}>
           <h2>{title}</h2>
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'foodie-course-image.s3.ap-southeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
-
 export default nextConfig;
